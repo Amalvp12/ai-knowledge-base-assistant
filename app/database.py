38120838aca6001b8b4b37b633d052e6281c41ff
Amalvp12 <vpamal7@gmail.com>
2026-06-08 22:02:39 +0530

    Added Neon DATABASE_URL configuration

--- app/database.py
+++ app/database.py
@@ -1,14 +1,7 @@
-import os
-from dotenv import load_dotenv
 import psycopg2
 
-load_dotenv()
-
 conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    database=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD")
+    "postgresql://neondb_owner:npg_rhRaLS9w8vlx@ep-dark-wave-aqgp65il.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 )
 
 cursor = conn.cursor()
